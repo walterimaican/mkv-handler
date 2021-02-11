aebc36a9307c46842517c7a43c041448d0512238
walterimaican <walwillwess@gmail.com>
2021-02-11 15:26:31 -0600

    subshift

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const waitForInput = async (query) => {
 const execWrapper = async (script) => {
     let returnStatus;
     await new Promise((resolve) => {
+        console.log(`Running: ${script}`);
         let resolveStatus = true;
 
         exec(script, (error, stdout, stderr) => {
@@ -64,6 +65,25 @@ const propEditPrompt = async () => {
     console.log();
     return;
 };
+
+const subtitlePrompt = async () => {
+    const filePath = await waitForInput('Path of srt file (eg. E:\\Desktop\\mySub.srt): ');
+    const shiftSyncResponse = await waitForInput('Shift or Sync? (Default: Shift): ');
+
+    if (shiftSyncResponse.toLowerCase() === 'sync') {
+        console.log('Syncing');
+        const startTime = await waitForInput('Correct time for first subtitle (eg. 00:01:30): ');
+        const endTime = await waitForInput('Correct time for last subtitle (eg. 02:30:00): ');
+        await execWrapper(`subshift sync ${filePath} ${startTime} ${endTime}`);
+    } else {
+        console.log('Shifting');
+        const shift = await waitForInput('Shift in milliseconds (eg. 1000 to delay 1 second or -1000 to advance 1 second): ');
+        await execWrapper(`subshift shift ${filePath} ${shift}`);
+    }
+    
+    console.log();
+    return;
+}
 
 (async () => {
     const separator = '========================================';
