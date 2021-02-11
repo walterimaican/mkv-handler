e7c3537512dcb06f42b277af8528f2601917be6c
walterimaican <walwillwess@gmail.com>
2021-02-11 17:18:23 -0600

    mergeSingleSubtitle

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { exec } = require('child_process');
+const path = require('path');
 const readline = require('readline');
 
 const waitForInput = async (query) => {
@@ -54,8 +55,66 @@ const infoPrompt = async () => {
     return;
 };
 
+// Need subtitleFile and inputFile; subtitleLanguage and outputFile are optional
+const mergeSingleSubtitle = async ({ inputFile, outputFile, subtitleFile, subtitleLanguage}) => {
+    // Sanitize
+    inputFile = inputFile.replace(/"/g, '');
+    outputFile = outputFile.replace(/"/g, '');
+    subtitleFile = subtitleFile.replace(/"/g, '');
+
+    // If no outputFile provided, overwrite inputFile
+    const shouldOverwrite = outputFile.length === 0 ? true : false;
+    const output = shouldOverwrite 
+        ? path.join(path.dirname(inputFile), `temp_${path.basename(inputFile)}`)
+        : outputFile;
+
+    const flags = subtitleLanguage.length === 0 ? '' : `--language 0:${subtitleLanguage}`;
+    const mergeScript = `mkvmerge -o "${output}" "${inputFile}" ${flags} "${subtitleFile}"`;
+
+    console.log();
+    await execWrapper(mergeScript);
+    await execWrapper(`rm -f "${inputFile}"`);
+    await execWrapper(`rm -f "${subtitleFile}"`);
+
+    if (shouldOverwrite) {
+        const overwriteScript = `mv -f "${output}" "${inputFile}"`;
+        await execWrapper(overwriteScript);
+    }
+};
+
+const mergeMultipleSubtitles = async () => {};
+
+const mergeSubtitles = async () => {
+    const singleMultiResponse = await waitForInput('Merge subtitles for a single file or multiple files? (s || m): ');
+
+    if (singleMultiResponse.toLowerCase() === 's') {
+        const inputFile = await waitForInput('Path of input MKV (eg. E:\\Desktop\\myMKV.mkv): ');
+        const subtitleFile = await waitForInput('Path of input subtitle file (eg. E:\\Desktop\\mySub.srt): ');
+        const subtitleLanguage = await waitForInput('Optional subtitle language (eg. jpn | Provide none for unlabeled track): ');
+        const outputFile = await waitForInput('Path of output MKV (eg. E:\\Desktop\\myNewMKV.mkv | Provide none to overwrite input MKV): ');
+        await mergeSingleSubtitle({ inputFile, outputFile, subtitleFile, subtitleLanguage });
+    } else if (singleMultiResponse.toLowerCase() === 'm') {
+        await mergeMultipleSubtitles();
+    } else {
+        console.error(`Command '${singleMultiResponse}' not recognized.\n`)
+    }
+};
+
 const mergePrompt = async () => {
-    await execWrapper('mkvmerge');
+    const subtitlesPrompt = '(s) Merge Subtitles\n';
+    const mergeMenu = 'What would you like to do?\n'
+    + subtitlesPrompt
+    + '> ';
+    const menuChoice = await waitForInput(mergeMenu);
+    switch(menuChoice) {
+        case 's':
+            await mergeSubtitles();
+            break;
+        default:
+            console.error(`Command '${response}' not recognized.\n`);
+            break;
+    }
+
     console.log();
     return;
 };
@@ -130,7 +189,7 @@ const subtitlePrompt = async () => {
                 stillUsing = false;
                 break;
             default:
-                console.log(`Unknown command: '${response}'.\n`);
+                console.error(`Command '${response}' not recognized.\n`);
                 break;
         }
     }
